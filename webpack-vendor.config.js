26b162c6c5add9aaa93b0ad4788f1e3d3dab18b0
stefan <ssavu@jahia.com>
2018-10-31 10:56:10 -0400

    Updated webpack configuration to use SystemJS

---
+++ webpack-vendor.config.js
@@ -0,0 +1,35 @@
+let SystemJSPlugin = require('webpack-systemjs-bundle-plugin/');
+let config = {
+    //The vendor bundle will be constructed and tracked by javascript loader in order to prevent loading the
+    // same dependency twice
+    entry: {
+        vendors: [
+            "react",
+            "react-dom"
+        ]
+    },
+
+    externals: {},
+
+    output: {
+        path         : __dirname + '/src/main/resources/javascript/bundles/',
+        filename     : "[name].js",
+        libraryTarget: "amd",
+        library      : "[name]_[hash]"
+    },
+
+    resolve:{
+        mainFields:["browser","main"]
+    },
+
+    plugins: [
+        new SystemJSPlugin({
+            path: __dirname + "/src/main/resources/javascript/bundles/[name].config.json",
+            name: "[name]_[hash]"
+        })
+    ],
+    devtool: 'source-map',
+    mode:'development'
+};
+
+module.exports = [config];
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,54 @@
 const path = require('path');
-
+let SystemJSPlugin = require('webpack-systemjs-bundle-plugin/');
+let nodeExternals = require('webpack-node-externals');
 module.exports = {
     entry: {
-        apps: [ path.resolve(__dirname, 'src/main/javascript/apps/banner/main.jsx')]
-    },
-    output: {
-        path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
-        filename: 'apps.js'
+        apps: [ path.resolve(__dirname, 'src/main/javascript/apps/banner/main')]
     },
     resolve: {
         mainFields: ['module', 'main'],
-        extensions: ['.mjs', '.js', '.jsx', 'json']
+        extensions: ['.mjs', '.js', '.jsx', '.json']
     },
+    externals: [
+        nodeExternals( { importType:'amd'})
+    ],
+
+    output: {
+        path: __dirname + '/src/main/resources/javascript/bundles/',
+        filename: "[name].js",
+        libraryTarget: "amd",
+        library: "[name]_[hash]"
+    },
+
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                include: [path.join(__dirname, "src")],
-                loader: 'babel-loader'
-            },
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        "presets": [["@babel/preset-env", {"modules": false}], "@babel/preset-react"],
+                        "plugins": [
+                            ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                            "@babel/plugin-proposal-function-sent",
+                            "@babel/plugin-proposal-export-namespace-from",
+                            "@babel/plugin-proposal-numeric-separator",
+                            "@babel/plugin-proposal-throw-expressions",
+                            "@babel/plugin-proposal-class-properties",
+                            "@babel/plugin-proposal-object-rest-spread",
+                        ]
+                    }
                 }
             }
-        }
+        ]
     },
+    plugins: [
+        new SystemJSPlugin({
+            path: __dirname + "/src/main/resources/javascript/bundles/[name].config.json",
+            name: "[name]_[hash]"
+        })
+    ],
     mode: 'development',
     devtool: 'source-map'
 };
